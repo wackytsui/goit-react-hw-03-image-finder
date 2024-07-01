c5cd7a793e4aa92287ee464b7f4e3833d0fe9fea
wackytsui <allan.altavano@gmail.com>
2024-07-01 22:17:31 +0800

    10

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,14 +1,10 @@
+import axios from 'axios';
+
 export const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43109028-0b309b2cc59a3e8f1dfb33466';
 
-export const options = {
-  params: {
-    key: API_KEY,
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: 1,
-    per_page: 40,
-  },
-};
+export const getAPI = async (query, page) => {
+  const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+  const response = await axios.get(url);
+  return response.data;
+}
